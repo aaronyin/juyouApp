d5518bb8ac82cb9b79aadfc30afd2904c14eb5c3
aaronyin <yiya820@163.com>
2018-06-11 23:38:27 +0800

    update

--- juyou/app/src/main/java/com/juyou/app/activity/LocalMerchantsActivity.java
+++ juyou/app/src/main/java/com/juyou/app/activity/LocalMerchantsActivity.java
@@ -1,15 +1,37 @@
 package com.juyou.app.activity;
 
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.juyou.app.R;
+import com.zhy.adapter.abslistview.CommonAdapter;
+import com.zhy.adapter.abslistview.ViewHolder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LocalMerchantsActivity extends BaseActivity {
 
+
+    @BindView(R.id.gv_merchants_type)
+    GridView gvMerchantsType;
+    @BindView(R.id.lv_more_shops)
+    ListView lvMoreShops;
+
+    private List<Integer> merchantsTypeList = new ArrayList<>();
+    private List<Map<String, Object>> moreShopsList = new ArrayList<>();
 
     @Override
     protected int setLayoutId() {
@@ -29,6 +51,24 @@ public class LocalMerchantsActivity extends BaseActivity {
     @Override
     protected void initData() {
 
+
+        TypedArray merchantsTypsArray = this.getResources().obtainTypedArray(R.array.merchants_type_icons);
+        for (int i = 0; i < merchantsTypsArray.length(); i++) {
+            merchantsTypeList.add(merchantsTypsArray.getResourceId(i, R.mipmap.icon_fscd));
+        }
+        merchantsTypsArray.recycle();
+
+
+        for (int i = 0; i < 10; i++) {
+            Map<String, Object> itemMap = new HashMap<>();
+            itemMap.put("img_shop", R.mipmap.shop_img);
+            itemMap.put("shop_title", "小食后中餐厅");
+            itemMap.put("shop_sub_title", "美食餐饮-中餐厅");
+            itemMap.put("btn_shop", "券");
+            itemMap.put("shop_address", "龙泉镇富民路32号（fdsafas ）");
+            itemMap.put("shop_distance", "1.23km");
+            moreShopsList.add(itemMap);
+        }
     }
 
     @Override
@@ -37,6 +77,26 @@ public class LocalMerchantsActivity extends BaseActivity {
         setTitleName(getString(R.string.local_merchants), Color.WHITE);
 //        setTitleRightText("申请入驻", Color.WHITE);//右侧文字
         showTitleRes(R.id.title_scan_code, R.id.title_application_in);
+
+        gvMerchantsType.setAdapter(new CommonAdapter<Integer>(this.getApplicationContext(), R.layout.item_list_merchants_type, merchantsTypeList) {
+            @Override
+            protected void convert(ViewHolder viewHolder, Integer item, int position) {
+                viewHolder.setImageResource(R.id.img_merchants_type, item);
+            }
+        });
+
+
+        lvMoreShops.setAdapter(new CommonAdapter<Map<String, Object>>(this.getApplicationContext(), R.layout.item_list_more_shops, moreShopsList) {
+            @Override
+            protected void convert(ViewHolder viewHolder, Map<String, Object> item, int position) {
+                viewHolder.setImageResource(R.id.img_shop, Integer.valueOf(item.get("img_shop")+""));
+                viewHolder.setText(R.id.tv_shop_title, item.get("shop_title")+"");
+                viewHolder.setText(R.id.tv_shop_sub_title, item.get("shop_sub_title")+"");
+                viewHolder.setText(R.id.btn_shop, item.get("btn_shop")+"");
+                viewHolder.setText(R.id.tv_shop_address, item.get("shop_address")+"");
+                viewHolder.setText(R.id.tv_shop_distance, item.get("shop_distance")+"");
+            }
+        });
     }
 
     @Override
@@ -52,7 +112,7 @@ public class LocalMerchantsActivity extends BaseActivity {
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.title_scan_code://扫描
                 Toast.makeText(this, "点击了扫描", Toast.LENGTH_SHORT).show();
                 break;
@@ -63,4 +123,5 @@ public class LocalMerchantsActivity extends BaseActivity {
         }
         return super.onMenuItemClick(item);
     }
+
 }
